6b5181351a8331f483f10edad8e5aa3af7869753
Constance <nozepastabos@gmail.com>
2023-05-13 23:51:12 +0200

    bonne US s

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 21:30:09 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/05/13 23:46:02 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/05/13 23:51:01 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,30 @@ char *ft_strjoin(const char *s1, const char *s2)
 		res[i++] = s2[j++];
 	res[i] = '\0';
 	return (res);
+}
+
+char	ft_strchr(const char *s, int c)
+{
+	int		cnt;
+	char	car;
+
+	car = c;
+	cnt = 0;
+	while (s[cnt] != car)
+	{
+		if (s[cnt] == '\0')
+			return (NULL);
+		cnt++;
+	}
+	return ((char *)s + cnt);
+}
+
+size_t ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
