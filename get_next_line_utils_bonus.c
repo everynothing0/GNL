464f169fd138979ba7333e2464baf93c148edc86
Constance <nozepastabos@gmail.com>
2023-06-02 14:40:12 +0200

    gnl utild bonud givr a shit melbourne

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 21:30:09 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/06/01 21:06:09 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/06/02 14:39:29 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,47 @@
 char	*ft_strjoin(char *s1, char *s2)
 {
 	size_t	i;
-	size_t	j;
-	char	*res;
+	size_t	c;
+	char	*str;
 
-	i = 0;
-	j = 0;
-	res = malloc(sizeof(char) * (ft_strlen(s1) + (ft_strlen(s2) + 1)));
-	if (!res || !s1 || !s2)
-		return (NULL);
-	while (s1[i] != '\0')
+	if (!s1)
 	{
-		res[i] = s1[i];
-		i++;
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
 	}
-	while (s2[j] != '\0')
-		res[i++] = s2[j++];
-	res[i] = '\0';
-	return (res);
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	c = 0;
+	if (s1)
+		while (s1[i++] != '\0')
+			str[i] = s1[i];
+	while (s2[c] != '\0')
+		str[i++] = s2[c++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
 }
 
 char	*ft_strchr(char *s, int c)
 {
-	int		cnt;
-	char	car;
+	int	i;
 
-	car = c;
-	cnt = 0;
-	while (s[cnt] != car)
+	i = 0;
+	if (!s)
+		return (0);
+	if (c == '\0')
+		return ((char *)&s[ft_strlen(s)]);
+	while (s[i] != '\0')
 	{
-		if (s[cnt] == '\0')
-			return (NULL);
-		cnt++;
+		if (s[i] == (char) c)
+			return ((char *)&s[i]);
+		i++;
 	}
-	return ((char *)s + cnt);
+	return (0);
 }
 
 size_t	ft_strlen(char *str)
@@ -55,7 +63,7 @@ size_t	ft_strlen(char *str)
 	size_t	i;
 
 	i = 0;
-	while (str[i])
+	while (str[i] != '\0')
 		i++;
 	return (i);
 }
